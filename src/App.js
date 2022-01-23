6aade8ace847741a66d2df96ddf7d852409a7f61
couldntfindabetterusername <bt20ece098@iiitn.ac.in>
2022-01-23 15:49:08 +0530

    navbar started

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
-import "./App.css";
+import Navbar from "./components/navbar/Navbar";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import Home from "./pages/home/Home";
+import Education from "./pages/education/Education";
+import Skills from "./pages/skills/Skills";
+import Work from "./pages/work/Work";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" exact element={<Home />}></Route>
+        <Route path="/education" exact element={<Education />}></Route>
+        {/* <Route path="/skills" exact>
+          <Skills></Skills>
+        </Route>
+        <Route path="/work" exact>
+          <Work></Work>
+        </Route> */}
+
+        <Navigate to="/" />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
