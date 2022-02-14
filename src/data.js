6de730a15741c4a1cbffacb17e7f1802aa57fd92
couldntfindabetterusername <bt20ece098@iiitn.ac.in>
2022-02-14 21:08:42 +0530

    socials section partially completed

---
+++ src/data.js
@@ -0,0 +1,33 @@
+const userData = {
+  socials: [
+    {
+      name: "linkedin",
+      iconLink: "https://cdn.worldvectorlogo.com/logos/linkedin-icon-2.svg",
+      link: "https://www.linkedin.com/in/sujal-barsaiyan-51a1b811b/",
+    },
+    {
+      name: "instagram",
+      iconLink: "https://cdn.worldvectorlogo.com/logos/instagram-2-1.svg",
+
+      link: "https://www.instagram.com/bettercallsujal/",
+    },
+    {
+      name: "github",
+      iconLink: "https://cdn.worldvectorlogo.com/logos/github-icon-1.svg",
+
+      link: "https://github.com/couldntfindabetterusername",
+    },
+    {
+      name: "behance",
+      iconLink: "https://cdn.worldvectorlogo.com/logos/behance-1.svg",
+      link: "https://www.behance.net/sujal-barsaiyan",
+    },
+    {
+      name: "dribbble",
+      iconLink: "https://cdn.worldvectorlogo.com/logos/dribbble-icon.svg",
+      link: "https://dribbble.com/sujalbarsaiyan",
+    },
+  ],
+};
+
+export default userData;
